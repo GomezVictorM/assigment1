26f66cc37b8a58790853d71f3a316a360d93117e
GomezVictorM <victor.gomcha@gmail.com>
2020-06-24 23:23:00 +0200

    Square roots ready

--- src/vg222et_assign1/SquareRoot.java
+++ src/vg222et_assign1/SquareRoot.java
@@ -1,7 +1,32 @@
 package vg222et_assign1;
 
+/* This program prompts user to enter an integer value for n, iterates through the Babylonian algorithm
+    until the guess is within 1% of the previous guess and outputs the answer as a real number to two decimal places.
+*/
+
+import java.util.Scanner;
+
 public class SquareRoot {
-    public static void calculateSquareRoot() {
-        System.out.println("Square root");
+    public static double calculateSquareRoot() {
+        double error = 0.01;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer to estimate the square roots");
+        double number = scanner.nextDouble();
+        double guessedNumber = (number / 2);
+        if (number <= 0) { // Number > 0 inorder to avoid errors, this program does not calculate imaginary numbers
+            System.out.println("\033[31;1mThe number has to be positive, please re-reun the program and try again\033[0m\n");
+        } else {
+
+            while (guessedNumber - (number / guessedNumber) > error) {
+                guessedNumber = (guessedNumber + (number / guessedNumber)) / 2;
+                System.out.println("\033[36;1m" + guessedNumber + "\033[0m");
+                System.out.println("Error" + (guessedNumber - (number / guessedNumber)));
+            }
+        }
+                System.out.println("Error" + (guessedNumber - (number / guessedNumber)));
+                return guessedNumber;
+
+
     }
 }
